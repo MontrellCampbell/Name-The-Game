5ff2f90f15f5d49c5e724c864352a0ac59390859
Zoobop <86850448+Zoobop@users.noreply.github.com>
2023-10-26 19:22:39 -0500

    Cleaned folder structure and tested javascript session storage

---
+++ js/gameDetails.js
@@ -0,0 +1,25 @@
+
+
+// When page loads
+function onLoad() {
+    console.log('Loaded');
+
+    const queryString = sessionStorage.getItem("search-query");
+    console.log(queryString);
+
+    // Ideal way to retrieve data from endpoint
+    // const data = getGame(queryString);
+    //    
+    //    Object will contain these fields:
+    //    
+    //    id: 0,
+    //    cover: null,
+    //    title: "",
+    //    description: "",
+    //    ageRating: "",
+    //    totalRating: 0,
+    //    category: "",
+    //    releaseDate: "1900-01-01"
+    //    [videos]
+    //
+}
--- js/index.js
+++ js/index.js
@@ -1,18 +1,37 @@
-function onInput(e) {
+// Callback for when the side menu button is clicked
+function showSideMenu(e) {
+    const sideMenu = document.querySelector("#SideMenu");
+    sideMenu.classList.toggle('active');
+}
+
+// Callback for when the search bar input text changes
+function onSearchBarInput(e) {
     console.log(e.target.value);
 }
 
-function onSubmit(e) {
-    console.log(e.target.value);
+// Callback for when the search bar gets submitted
+function onSearchBarSubmit(e) {
+    // Return early if nothing in search bar
+    const queryString = e.target.value;
+    if (queryString === '' || queryString === null)
+        return;
+
+    // Save string to session storage
+    sessionStorage.setItem("search-query", e.target.value);
+
+    location.href = "./gameDetails.html";
 }
 
 // When page loads
 function onLoad() {
-    
     console.log('Loaded');
 
-    const searchBarElement = document.querySelector("#search-bar");
-    searchBarElement.addEventListener("input", onInput);
-    searchBarElement.addEventListener("change", onSubmit);
+    // Assign side menu button event callbacks
+    const sideMenuButtonElement = document.querySelector("#side-menu-button");
+    sideMenuButtonElement.addEventListener("click", showSideMenu);
 
+    // Assign search bar event callbacks
+    const searchBarElement = document.querySelector("#search-bar");
+    searchBarElement.addEventListener("input", onSearchBarInput);
+    searchBarElement.addEventListener("change", onSearchBarSubmit);
 }
